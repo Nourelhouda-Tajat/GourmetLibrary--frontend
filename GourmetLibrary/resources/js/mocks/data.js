0f1da221fdebf03c09d7400982b07197388fc650
youcode safi <tajat.nourelhouda@gmail.com>
2026-04-06 22:43:37 +0100

    test: add mock data for categories and books

---
+++ GourmetLibrary/resources/js/mocks/data.js
@@ -0,0 +1,10 @@
+export const mockCategories = [
+    { id: 1, name: "Pâtisserie Française" },
+    { id: 2, name: "Cuisine Italienne" },
+    { id: 3, name: "Plats Végétariens" }
+];
+
+export const mockBooks = [
+    { id: 1, title: "Le Grand Manuel du Pâtissier", author: "Mélanie Dussert", category_id: 1, total_copies: 5 },
+    { id: 2, title: "Pasta Basta", author: "Luigi Riva", category_id: 2, total_copies: 3 }
+];
