68727a37657a039aecf7479e2b90f9e25783fe0e
youcode safi <tajat.nourelhouda@gmail.com>
2026-04-06 22:51:23 +0100

    feat: setup main App component and routing

--- GourmetLibrary/resources/js/app.jsx
+++ GourmetLibrary/resources/js/app.jsx
@@ -1,1 +1,21 @@
-import './bootstrap';
+import "./bootstrap";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Home from "./pages/Home";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* Ajoute d'autres routes ici plus tard */}
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
